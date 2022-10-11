628b7ade318323fc482762f068c25e1af28ccb4f
Vadim Gaidukov <vgaidukov@gmail.com>
2022-10-12 00:11:12 +0300

    feat: add Helmet protection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
-const express = require('express');
 const mongoose = require('mongoose');
+const express = require('express');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
@@ -16,6 +17,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(corsHandler);
